e1f83b6a1a613c29db7585856890387c9806b0d1
XingMengL <1156351528@qq.com>
2020-05-24 23:05:46 +0800

    list\list.cpp

--- list/list.cpp
+++ list/list.cpp
@@ -156,6 +156,63 @@ void Test6()
 		cout<<ch<<" ";
 	cout<<endl;
 }
+// 迭代器失效问题
+void Test7()
+{
+	list<int> L;
+	L.push_back(1);
+	L.push_back(2);
+	L.push_back(3);
+	L.push_back(4);
+	L.push_back(5);
+
+	auto it = L.begin();
+
+	/*
+		删除前迭代器it指向的时第一个节点
+		节点被删除后，此时迭代器it指向的是一个已经被释放的节点
+		此时的迭代器已经失效了
+		如果使用的话要重新给迭代器赋值,不然会崩溃
+	*/
+
+	it = L.erase(L.begin());  // 方法2
+	
+	// erase的返回值是当前删除节点的下一个节点的位置
+    
+	
+	//it = L.begin();  //方法1
+
+
+	while(it != L.end())
+	{
+		cout<<*it<<" ";
+		it++;
+	}
+	cout<<endl;
+	
+}
+
+void Test8()
+{
+	
+	int array1[] = {1,2,4,6,2,6,7,3,9,20,40,80};
+	list<int> L(array1,array1+sizeof(array1)/sizeof(array1[0]));
+	
+	//删除所有的偶数
+	auto it = L.begin();
+	while(it != L.end())
+	{
+		if(*it%2 == 0)
+		
+			it = L.erase(it);	
+		else
+		    it++;
+	}
+	for(auto ch: L)
+		cout<<ch<<" ";
+	cout<<endl;
+}
+
 int main()
 {
 	//Test1();
@@ -163,8 +220,9 @@ int main()
 	//Test3();
 	//Test4();
 	//Test5();
-	Test6();
-
+	//Test6();
+	//Test7();
+	Test8();
 	system("pause");
 	return 0;
 }
