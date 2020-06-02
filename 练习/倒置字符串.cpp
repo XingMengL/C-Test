081eec8a4156380c6f19a7f92d4511b09cf86340
XingMengL <1156351528@qq.com>
2020-06-02 22:20:32 +0800

    练习\倒置字符串.cpp

---
+++ 练习/倒置字符串.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespeace std;
+
+#include <string>
+#include <vector>
+int main()
+{
+	vector<string> v;
+    string s1;
+
+    while(cin >> s1)
+    {
+        v.push_back(s2);
+    }
+    for(int i = 0; i < v.size()-1; i--)
+    {
+        if(i == 0)
+        {
+        	cout << v[i] <<endl;
+		}
+		else
+		{
+			cout<< v[i] <<" ";
+		}
+		
+    }
+    
+	
+	
+	return 0;
+ } 
