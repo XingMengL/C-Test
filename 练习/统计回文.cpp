d849bd84ba5e7636b7650ba5a017c5c4a2016b5e
XingMengL <1156351528@qq.com>
2020-06-05 14:55:57 +0800

    练习\统计回文.cpp

---
+++ 练习/统计回文.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include <string>
+
+int main()
+{
+    string s1,s2;
+    string s3,s4;
+    cin >> s1 >> s2;
+    s4 = s1;
+    int c = s1.length()+1;
+    int count = 0;
+    //循环插入
+    while(c--)
+    {   
+        s1.insert(c,s2);
+       //判断是否为回文
+        s3 = s1;
+     reverse(s1.begin(),s1.end());
+        if(s3 == s1)
+        {
+            count++;
+        }
+        
+        s1 = s4;
+    }
+    cout<<count<<endl;
+        return 0;
+}
